4f28c11629cda18f975eef32fd9def49375ffac4
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-05-18 14:02:24 +0700

    Update pages-routing.module.ts

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -54,7 +54,7 @@ const routes: Routes = [
       {
         path: 'statistics',
         component: StatisticsComponent,
-        data: { breadcrumb: 'users' },
+        data: { breadcrumb: 'statistics' },
       },
     ],
   },
