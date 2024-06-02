327dd2e009bb935fee19f3721d579e306f7780c6
Tranthikimdang <dangttkpc05468@fpt.edu.vn>
2024-06-02 21:40:17 +0700

    xoa comment

---
+++ admin/src/app/@core/model/comment.model.ts
@@ -0,0 +1,9 @@
+export class CommentModel {
+  commentsId: number;
+  userName: string;
+  commentsEmail: string;
+  productName: string;
+  imageUrl: string;
+  commentsContent: string;
+  commentsDate: string;
+}
