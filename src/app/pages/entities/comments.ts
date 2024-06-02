a461b17b3f259530e47635e7e921d2dc0a02186c
phinhpc05786 <phinhpc05786@fpt.edu.vn>
2024-06-02 21:59:37 +0700

    đừng gộp cái này nha nó lỗi

--- src/app/pages/entities/comments.ts
+++
@@ -1,9 +0,0 @@
-export interface Icomments {
-    commentsId: number;
-    userName: string;
-    commentsEmail: string;
-    productName: string;
-    imageUrl: string;
-    commentsContent: string;
-    commentsDate: string;
-   }
